926f521b7c0244316bfaaec7a4394b5b9a9f9e47
ImBobby <squidz.bee@gmail.com>
2014-09-30 12:40:36 +0700

    add email validation

--- baze.validate.js
+++ baze.validate.js
@@ -6,6 +6,13 @@ var BazeValidate = (function ($) {
     classMsg    : 'form-msg-error'
   };
 
+
+  /**
+   * ---------------------- *
+   * Public functions & API *
+   * ---------------------- *
+   */
+
   var setClassError = function ( _class ) {
     options.classError = _class;
 
@@ -24,43 +31,55 @@ var BazeValidate = (function ($) {
     return this;
   };
 
-  function run() {
-    var forms   = $('form[data-baze-validate]');
+  var run = function () {
+    var forms = $('form[data-baze-validate]');
 
     if ( !forms.length ) return;
 
     forms
       .attr('novalidate', '')
       .submit( validateFields );
-  }
+  };
+
+
+  /**
+   * -------------- *
+   * Core functions *
+   * -------------- *
+   */
 
   function validateFields( evt ) {
     var _this   = $(this),
-
-        // Required inputs in form
-        fields  = _this.find('[required]'),
-
+        invalid = [],
         // Flag
-        isOK    = true,
+        isOK;
 
-        // Store invalid inputs
-        invalid = [];
+    isOK = validateEmpty( _this, invalid );
+
+    if ( !isOK ) evt.preventDefault();
+
+    isOK = validateEmail( _this, invalid );
+
+    if ( !isOK ) {
+      invalid[0].focus();
+      evt.preventDefault();
+    }
+  }
+
+  function validateEmpty( form, invalid ) {
+    var fields  = form.find('[required]'),
+        isOK    = true;
 
     resetClass( fields );
-    clearMessage( _this );
+    clearMessage( form );
 
-    for (var i = 0; i < fields.length; i++) {
+    for ( var i = 0; i < fields.length; i++ ) {
       var curr = fields.eq(i);
 
       if ( curr.val() === '' || curr.val() === null ) {
-        var msg = $(document.createElement('span'));
-
-        msg
-          .addClass( options.classMsg )
-          .text('This field is required')
-          .insertAfter( curr );
-
         curr.addClass( options.classError );
+
+        addMessage( curr, 'This field is required.' );
 
         invalid.push(curr);
 
@@ -73,10 +92,66 @@ var BazeValidate = (function ($) {
     }
 
     if ( !isOK ) {
-      // Set focus to the first invalid input
-      invalid[0].focus();
-      
-      evt.preventDefault();
+      return false;
+    }
+
+    return true;
+  }
+
+  function validateEmail( form, invalid ) {
+    var fields  = form.find('[type="email"]'),
+        isOK    = true;
+
+    if ( !fields.length ) return;
+
+    resetClass( fields );
+
+    for ( var i = 0; i < fields.length; i++ ) {
+      var curr    = fields.eq(i),
+          currVal = curr.val();
+
+      if ( !isEmailValid( currVal ) ) {
+        curr.addClass( options.classError );
+
+        addMessage( curr, 'Invalid email address.' );
+
+        invalid.push(curr);
+
+        isOK = false;
+      } else {
+        curr
+          .removeClass( options.classError )
+          .addClass( options.classSuccess );
+      }
+    }
+
+    if ( !isOK ) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  /**
+   * ---------------- *
+   * Helper functions *
+   * ---------------- *
+   */
+
+  function addMessage ( elem, message ) {
+    var exist = elem.parent().find('.' + options.classMsg),
+        msg;
+    
+    if ( exist.length ) {
+      exist.text( message );
+    } else {
+      msg = $(document.createElement('span'));
+
+      msg
+        .addClass( options.classMsg )
+        .text( message )
+        .insertAfter( elem );
     }
   }
 
@@ -90,6 +165,11 @@ var BazeValidate = (function ($) {
     if ( !msg.length ) return;
 
     msg.remove();
+  }
+
+  function isEmailValid( email ) {
+    // http://badsyntax.co/post/javascript-email-validation-rfc822
+    return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( email );
   }
 
   return {
