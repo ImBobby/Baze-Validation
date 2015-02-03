4133218039fe29a24a2d2a7cca281ca6aaeff141
ImBobby <squidz.bee@gmail.com>
2015-02-03 15:16:21 +0700

    code update

--- baze.validate.v1.js
+++ baze.validate.v1.js
@@ -1,10 +1,6 @@
-
-
 ;(function ( $, window, document, undefined ) {
 
-  function log( msg ) {
-    console.log( msg );
-  }
+  var pluginName = 'BazeValidate';
 
   var defaults = {
     classError  : 'form-input--error',
@@ -16,102 +12,168 @@
     msgNumber   : 'Input must be number.'
   };
 
-  function BazeValidate ( element, userOptions ) {
-    plugin          = this;
-    plugin._elem    = $(element);
-    plugin._opts    = $.extend({}, defaults, userOptions);
-    plugin._inputs  = plugin._elem.find('[required]');
+  function Plugin ( element, options ) {
+    this.element    = element;
+    this.$element   = $(element);
+    this.settings   = $.extend( {}, defaults, options );
+    this._defaults  = defaults;
+    this._name      = pluginName;
 
-    plugin._setNovalidate();
-    plugin._registerEvent();
+    this.init();
   }
 
-  $.extend( BazeValidate.prototype, {
+  Plugin.prototype.init = function () {
+    this.element.setAttribute('novalidate', '');
+    this.$element.find('[required]').attr('aria-required', 'true');
 
-    _init: function () {
-      plugin._setNovalidate();
-    },
+    var userOpts = this.settings;
 
-    _setNovalidate: function () {
-      plugin._elem.attr('novalidate', '');
-    },
+    var clearAllMessages = function ( form ) {
+      var msgs = form.find('.' + userOpts.classMsg);
 
-    _registerEvent: function () {
-      var _this   = plugin._elem,
-          _opts   = plugin._opts,
-          valid   = true;
+      $(msgs).remove();
+    };
 
-      _this.submit(function (e) {
-        valid = BazeValidate.prototype._validateEmpty( _this, _opts );
+    var addMessage = function ( field, message ) {
+      var hasMsg  = field.parent().find('.' + userOpts.classMsg);
 
-        if ( !valid ) e.preventDefault();
+      if ( hasMsg.length ) {
+        hasMsg.remove();
+      }
+
+      var id      = getUID();
+      var msg     = $(document.createElement('span'));
+
+      field.attr({
+        'aria-describedBy': id,
+        'aria-invalid': 'true'
       });
-    },
 
-    _validateEmpty: function ( form, options ) {
-      var inputs  = form.find('[required]'),
-          $inputs = $(inputs),
-          isOK    = true;
+      msg
+        .addClass( userOpts.classMsg )
+        .attr('id', id)
+        .text( message )
+        .insertAfter( field );
+    };
 
-      BazeValidate.prototype._resetClass( $inputs, options );
-      BazeValidate.prototype._clearMessage( form, options );
+    var resetFields = function ( fields ) {
+      fields
+        .removeAttr('aria-invalid aria-describedBy')
+        .removeClass( userOpts.classError )
+        .removeClass( userOpts.classSuccess );
+    };
 
-      for (var i = 0, j = inputs.length; i < j; i++) {
-        var $current  = inputs.eq(i),
-            value     = $current.val();
+    var validateEmpty = function ( fields ) {
+      var allIsWell = true;
+
+      fields.each( function () {
+        var $field  = $(this),
+            value   = $field.val();
 
         if ( value === '' || value === null || $.trim(value) === '' ) {
-          $current.addClass( options.classError );
-          BazeValidate.prototype._addMessage( $current, options, options.msgEmpty );
+          $field.addClass( userOpts.classError );
+          addMessage( $field, userOpts.msgEmpty );
 
-          isOK = false;
+          allIsWell = false;
         } else {
-          $current
-            .removeClass( options.classError )
-            .addClass( options.classSuccess );
+          $field.addClass( userOpts.classSuccess );
         }
+      });
+
+      return allIsWell;
+    };
+
+    var validateEmail = function ( fields ) {
+      var allIsWell = true,
+          type;
+
+      fields.each( function (i, el) {
+        var $el = $(el);
+        type = el.getAttribute('type');
+
+        if ( type === 'email' && !isEmailValid( el.value ) ) {
+          resetFields( $el );
+          $el.addClass( userOpts.classError );
+          addMessage( $el, userOpts.msgEmail );
+
+          allIsWell = false;
+        }
+      });
+
+      return allIsWell;
+    };
+
+    var validateNumeric = function ( fields ) {
+      var allIsWell = true,
+          type;
+
+      fields.each( function (i, el) {
+        var $el = $(el);
+        type = el.getAttribute('type');
+
+        if ( type === 'number' && !$.isNumeric( el.value ) ) {
+          resetFields( $el );
+          $el.addClass( userOpts.classError );
+          addMessage( $el, userOpts.msgNumber );
+
+          allIsWell = false;
+        }
+      });
+
+      return allIsWell;
+    };
+
+    var validateFields = function ( evt ) {
+      var $this   = $(this),
+          fields  = $this.find('[required]'),
+          isOK    = true,
+          focusedField,
+          msg;
+
+      resetFields( fields );
+      clearAllMessages( $this );
+
+      isOK = validateEmpty( fields );
+
+      if ( !isOK ) evt.preventDefault();
+
+      isOK = validateEmail( fields );
+
+      if ( !isOK ) evt.preventDefault();
+
+      isOK = validateNumeric( fields );
+
+      if ( !isOK ) {
+        evt.preventDefault();
+
+        console.log($this.find('.' + userOpts.classError).eq(0));
       }
+    };
 
-      return isOK;
-    },
+    this.$element.submit( validateFields );
+  };
 
-    _addMessage: function ( input, options, message ) {
-      var existingMsg = input.parent().find('.' + options.classMsg),
-          $msg;
 
-      if ( existingMsg.length ) {
-        existingMsg.text( message );
-      } else {
-        $msg = $(document.createElement('span'));
+  function isEmailValid( email ) {
+    // http://badsyntax.co/post/javascript-email-validation-rfc822
+    return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( email );
+  }
 
-        $msg
-          .addClass( options.classMsg )
-          .text( message )
-          .insertAfter( input );
+  function getUID() {
+    var randInt = Math.floor((new Date).getTime()) + Math.floor((Math.random() * 100));
+    var UID = 'BV' + randInt;
+
+    return UID;
+  }
+
+  $.fn[ pluginName ] = function ( options ) {
+    this.each(function() {
+      if ( !$.data( this, "plugin_" + pluginName ) ) {
+        $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
       }
-
-    },
-
-    _resetClass: function ( inputs, options ) {
-      inputs.removeClass( options.classError + ' ' + options.classSuccess );
-    },
-
-    _clearMessage: function ( form, options ) {
-      var msg = form.find('.' + options.classMsg);
-
-      msg.remove();
-    }
-
-  });
-
-  $.fn.bazeValidate = function ( userOptions ) {
-
-    if ( !this.length ) return;
-
-    return this.each( function () {
-      new BazeValidate( this, userOptions );
     });
 
+    return this;
   };
 
 })( jQuery, window, document );
