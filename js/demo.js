da57035a554d071a3089d0a5fa73d6b663545f82
ImBobby <squidz.bee@gmail.com>
2015-02-07 20:01:23 +0700

    new documentation for v1.0.0

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,37 @@
+$(function () {
 
-(function ($) {
-    BazeValidate.run();
-})(window.jQuery);
+    var Site = {
+
+        init: function () {
+            this.initBazeValidate();
+            this.highlight();
+            this.titleParallax();
+        },
+
+        initBazeValidate: function () {
+            $('form').bazeValidate();
+        },
+
+        highlight: function () {
+            hljs.initHighlighting();
+        },
+
+        titleParallax: function () {
+            var header = document.querySelector('.main-header'),
+                checkpoint = header.clientHeight;
+
+            window.addEventListener('scroll', function ( evt ) {
+                var pos = window.scrollY;
+                var opacityVal = ((pos/200) - 1) * -1;
+
+                if ( pos < checkpoint ) {
+                    header.style.transform = 'translateY(' + pos/4*-1 + 'px)';
+                    header.style.opacity = opacityVal;
+                }
+            }, false);
+        }
+
+    };
+
+    Site.init();       
+});
